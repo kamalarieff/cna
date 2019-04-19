b1387a7bb87c8962c3231ee90b1483333e581618
kamalarieff <kamal.arieff@gmail.com>
2019-04-19 14:31:23 +0800

    Add missing Dispatch type

--- templates/saga/app/pages/index.tsx
+++ templates/saga/app/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Component as Home } from '../containers/Home';
 import { fetchData } from '../containers/Home/actions';
+import { Dispatch } from 'redux';
 
 type Props = {
   dispatch: Dispatch;
