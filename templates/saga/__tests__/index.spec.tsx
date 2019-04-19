43c61427660f85be83c94d9bb631e0b621dafb6e
kamalarieff <kamal.arieff@gmail.com>
2019-04-19 16:47:20 +0800

    Add unit tests

---
+++ templates/saga/__tests__/index.spec.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Component as Header } from '../app/components/Header';
+
+describe('test', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('Components', () => {
+  describe('Header', () => {
+    it('should match the snapshot correctly', () => {
+      const component = shallow(<Header />);
+      expect(component).toMatchInlineSnapshot();
+    });
+  });
+});
