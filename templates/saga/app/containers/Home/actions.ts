293d6a18597512c55622443b6dab8ed1d275f2d3
kamalarieff <kamal.arieff@gmail.com>
2019-04-22 12:02:36 +0800

    Reformat code

--- templates/saga/app/containers/Home/actions.ts
+++ templates/saga/app/containers/Home/actions.ts
@@ -4,8 +4,8 @@ export function fetchData(query: string | number) {
   return {
     type: FETCH_DATA,
     payload: {
-      query,
-    },
+      query
+    }
   };
 }
 
@@ -13,8 +13,8 @@ export function fetchDataSuccess(response: {}) {
   return {
     type: FETCH_DATA_SUCCESS,
     payload: {
-      response,
-    },
+      response
+    }
   };
 }
 
@@ -22,7 +22,7 @@ export function fetchDataFail(error: Error) {
   return {
     type: FETCH_DATA_FAIL,
     payload: {
-      error,
-    },
+      error
+    }
   };
 }
