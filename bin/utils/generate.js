56247c34c18638dc2b81e58de215e50fd5056d32
kamalarieff <kamal.arieff@gmail.com>
2019-04-22 17:42:26 +0800

    Export the store state type and import the global application store state

--- bin/utils/generate.js
+++ bin/utils/generate.js
@@ -8,10 +8,14 @@ module.exports = function(componentType) {
 
   const runPlop = spawn(plop, ['--plopfile', generator, componentType], {
     cwd: process.cwd(),
-    stdio: 'inherit'
+    stdio: 'inherit',
   });
 
   runPlop.on('exit', function() {
+    componentType === 'container' &&
+      console.log(
+        chalk.yellowBright("Don't forget to add your reducer to reducer.ts!")
+      );
     console.log(chalk.cyan('Completed task!!!'));
     process.exit(1);
   });
